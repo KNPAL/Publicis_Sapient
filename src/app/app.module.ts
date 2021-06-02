c81fead0c217916b8843af8a1ac92f74c3b46fda
unknown <14sudhansh@gmail.com>
2021-06-02 16:00:02 +0530

    remove unwanted component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TileComponent } from './components/tile/tile.component';
-import { ToggleIconComponent } from './components/toggle-icon/toggle-icon.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TileComponent,
-    ToggleIconComponent
+    TileComponent
   ],
   imports: [
     BrowserModule,
